94cf00bcf7ee7f1c01f5aef45f45d93081e81881
Louise Beh <louise.beh@gmail.com>
2015-02-24 18:07:35 +0000

    Added server and rspec test files

--- lib/server.rb
+++ lib/server.rb
@@ -5,3 +5,7 @@ env = ENV['RACK_ENV'] || 'development'
 DataMapper.setup(:default, "postgres://localhost/bookmark_manager_#{env}")
 
 require './lib/link'
+
+DataMapper.finalize
+
+DataMapper.auto_upgrade!
