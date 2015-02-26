1b372828187ae5d883a0e4ea61a031db5cd0cbb0
lorisfo <loris-fo@outlook.com>
2015-02-26 16:20:35 +0000

    Added a sign out feature with successful tests

--- app/server.rb
+++ app/server.rb
@@ -1,66 +1,21 @@
 require 'sinatra'
 require 'data_mapper'
 require 'rack-flash'
-require './app/models/link'
-require './app/models/tag'
-require './app/models/user'
+require 'sinatra/partial'
+require_relative 'models/link'
+require_relative 'models/tag'
+require_relative 'models/user'
 require_relative 'data_mapper_setup'
 require_relative 'helpers/application'
+
+require_relative 'controllers/users'
+require_relative 'controllers/sessions'
+require_relative 'controllers/links'
+require_relative 'controllers/tags'
+require_relative 'controllers/application'
+
 
 enable :sessions
 set :session_secret, 'super secret'
 use Rack::Flash
-
-  get '/' do
-    @links = Link.all
-    erb :index
-  end
-
-
-  get '/tags/:text' do
-    tag = Tag.first(:text => params[:text])
-    @links = tag ? tag.links : []
-    erb :index
-  end
-
-  get '/users/new' do
-    @user = User.new
-    erb :"users/new"
-  end
-
-  get '/sessions/new' do
-    erb :"sessions/new"
-  end
-
-  post '/links' do
-    url = params["url"]
-    title = params["title"]
-    tags = params["tags"].split(" ").map {|tag| Tag.first_or_create(:text => tag)}
-    Link.create(:url => url, :title => title, :tags => tags)
-    redirect to ('/')
-  end
-
-  post '/users' do
-    @user = User.create(:email => params[:email],
-                        :password => params[:password],
-                        :password_confirmation => params[:password_confirmation])
-    if @user.save
-      session[:user_id] = @user.id
-      redirect to ('/')
-    else
-      flash.now[:errors] = @user.errors.full_messages
-      erb :"/users/new"
-    end
-  end
-
-  post '/sessions' do
-    email, password = params[:email], params[:password]
-    user = User.authenticate(email, password)
-    if user
-      session[:user_id] = user.id
-      redirect to ('/')
-    else
-      flash[:errors] = ["The email or password is incorrect"]
-      erb :"sessions/new"
-    end
-  end
+set :partial_template_engine, :erb
