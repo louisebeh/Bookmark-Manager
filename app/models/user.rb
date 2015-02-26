b6ec51503a681bb0d329401569260dd43264eb22
lorisfo <loris-fo@outlook.com>
2015-02-26 11:45:09 +0000

    added an error message for unmatch passwords

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,7 @@ class User
   attr_reader :password
   attr_accessor :password_confirmation
 
-  validates_confirmation_of :password
+  validates_confirmation_of :password, :message => "Sorry, your passwords don't match"
   validates_uniqueness_of :email
 
   def password=(password)
