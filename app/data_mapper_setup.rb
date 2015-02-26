36308d5fc8a8fe5880e383a5d5244c9803d5a29f
Louise Beh <louise.beh@gmail.com>
2015-02-26 14:22:28 +0000

    Added feature: Signing in with successful tests

--- app/data_mapper_setup.rb
+++ app/data_mapper_setup.rb
@@ -4,4 +4,4 @@ DataMapper.setup(:default, "postgres://localhost/bookmark_manager_#{env}")
 
 DataMapper.finalize
 
-DataMapper.auto_upgrade!
+# DataMapper.auto_upgrade!
--- app/server.rb
+++ app/server.rb
@@ -16,6 +16,14 @@ use Rack::Flash
     erb :index
   end
 
+  post '/links' do
+    url = params["url"]
+    title = params["title"]
+    tags = params["tags"].split(" ").map {|tag| Tag.first_or_create(:text => tag)}
+    Link.create(:url => url, :title => title, :tags => tags)
+    redirect to ('/')
+  end
+
   get '/tags/:text' do
     tag = Tag.first(:text => params[:text])
     @links = tag ? tag.links : []
@@ -27,28 +35,35 @@ use Rack::Flash
     erb :"users/new"
   end
 
-  post '/links' do
-    url = params["url"]
-    title = params["title"]
-    tags = params["tags"].split(" ").map {|tag| Tag.first_or_create(:text => tag)}
-    Link.create(:url => url, :title => title, :tags => tags)
-    redirect to ('/')
-  end
-
   post '/users' do
-    @user = User.create( :email => params[:email],
+    @user = User.create(:email => params[:email],
                         :password => params[:password],
                         :password_confirmation => params[:password_confirmation])
     if @user.save
       session[:user_id] = @user.id
       redirect to ('/')
     else
-      flash.now[:errors]= @user.errors.full_messages
+      flash.now[:errors] = @user.errors.full_messages
       erb :"/users/new"
     end
-
-
   end
+
+  get '/sessions/new' do
+    erb :"sessions/new"
+  end
+
+  post '/sessions' do
+    email, password = params[:email], params[:password]
+    user = User.authenticate(email, password)
+    if user
+      session[:user_id] = user.id
+      redirect to ('/')
+    else
+      flash[:errors] = ["The email or password is incorrect"]
+      erb :"sessions/new"
+    end
+  end
+
 
 
 
